2d3705939e444bd0b66e5622089f3417b5efb447
Miroslav Adamec <miroslav.adamec@hotmail.com>
2024-10-07 21:15:48 +0200

    feat: add httpclient handler project

---
+++ src/Madev.Utils.Infrastructure.Http/HttpClientHandlers/AzureAdAppMessageHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+
+namespace Madev.Utils.Infrastructure.Http.HttpClientHandlers
+{
+    public class AzureAdAppMessageHandler : HttpClientHandler
+    {
+        private readonly TokenCredential _credential;
+        private readonly string[] _scopes;
+
+        public AzureAdAppMessageHandler(string resourceId)
+            : this(new DefaultAzureCredential(), resourceId)
+        {
+        }
+
+        public AzureAdAppMessageHandler(TokenCredential credential, string resourceId)
+        {
+            _credential = credential;
+            _scopes = new[] { resourceId + "/.default" };
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await _credential.GetTokenAsync(new TokenRequestContext(_scopes), cancellationToken);
+
+            request.Headers.Remove("Authorization");
+            request.Headers.Add("Authorization", "Bearer " + token);
+            return await base.SendAsync(request, cancellationToken)
+                .ConfigureAwait(false);
+        }
+    }
+}
