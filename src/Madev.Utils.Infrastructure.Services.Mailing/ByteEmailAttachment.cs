308ae71d5012ea6affdf19d10fbc9348acc666f5
kuberto773 <kuberto773@github.com>
2025-11-26 16:20:42 +0100

    chore: extended email attachment contract

--- src/Madev.Utils.Infrastructure.Services.Mailing/ByteEmailAttachment.cs
+++ src/Madev.Utils.Infrastructure.Services.Mailing/ByteEmailAttachment.cs
@@ -4,13 +4,28 @@ namespace Madev.Utils.Infrastructure.Services.Mailing
 {
     public class ByteEmailAttachment : IEmailAttachment
     {
-        public ByteEmailAttachment(string filename, byte[] content)
+        public ByteEmailAttachment(string filename, byte[] content, bool isInline = false, string? contentId = null, string? contentType = null)
         {
             Filename = filename ?? throw new ArgumentException($"{nameof(filename)} cannot be null.");
             Content = content ?? throw new ArgumentException($"{nameof(content)} cannot be empty.");
+            IsInline = isInline;
+            
+            if(IsInline && contentId == null)
+            {
+                throw new ArgumentException("If an attachment is inline, ContentId must be defined");
+            }
+            ContentId = contentId;
+
+            if(contentType != null)
+            {
+                ContentType = contentType;
+            }
         }
 
         public string Filename { get; }
         public byte[] Content { get; }
+        public string? ContentId { get; set; }
+        public bool IsInline { get; set;}
+        public string? ContentType { get; set; } = "application/octet-stream";
     }
 }
--- src/Madev.Utils.Infrastructure.Services.Mailing/FilepathEmailAttachment.cs
+++ src/Madev.Utils.Infrastructure.Services.Mailing/FilepathEmailAttachment.cs
@@ -4,11 +4,26 @@ namespace Madev.Utils.Infrastructure.Services.Mailing
 {
     public class FilepathEmailAttachment : IEmailAttachment
     {
-        public FilepathEmailAttachment(string path)
+        public FilepathEmailAttachment(string path, bool isInline = false, string? contentId = null, string? contentType = null)
         {
             Path = path ?? throw new ArgumentException($"{nameof(path)} cannot be empty.");
+            IsInline = isInline;
+
+            if (IsInline && contentId == null)
+            {
+                throw new ArgumentException("If an attachment is inline, ContentId must be defined");
+            }
+            ContentId = contentId;
+
+            if(contentType != null)
+            {
+                ContentType = contentType;
+            }
         }
 
         public string Path { get; }
+        public string? ContentId { get; set; }
+        public bool IsInline { get ; set ; }
+        public string? ContentType { get ; set ; } = "application/octet-stream";
     }
 }
