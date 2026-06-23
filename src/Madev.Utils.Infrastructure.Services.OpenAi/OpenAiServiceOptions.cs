098ed199d93942af99e3a6e83583f6465024dcfc
kuberto773 <kuberto773@github.com>
2026-06-23 14:42:51 +0200

    Revert "feat: add OpenAi service to infrastructure"
    This reverts commit a4ae1707a5837991a189a8c0c93deaf7e50e8f5b.

--- src/Madev.Utils.Infrastructure.Services.OpenAi/OpenAiServiceOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace Madev.Utils.Infrastructure.Services.OpenAi;
-
-public class OpenAiServiceOptions
-{
-    public string Endpoint { get; set; } = null!;
-    public string DeploymentModel { get; set; } = null!;
-    public string SystemChatMessage { get; set; } = null!;
-    public float? Temperature { get; set; }
-}
